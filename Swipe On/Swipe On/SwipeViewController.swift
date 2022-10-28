cde6f8093d31bdd7a8b5bfdb8051939a4d32b136
Arjun Mohan <62532677+vparjunmohan@users.noreply.github.com>
2022-10-29 04:08:30 +0530

    left and right swipe added

--- Swipe On/Swipe On/SwipeViewController.swift
+++ Swipe On/Swipe On/SwipeViewController.swift
@@ -11,9 +11,113 @@ class SwipeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(addSwipeView())
     }
+    
+    func addSwipeView() -> UIView {
+        let swipeView = UIView()
+        let color = UIColor.random()
+        let viewHeight = 350.0
+        let viewWidth = 250.0
+        let defaults = UserDefaults.standard
+        if let viewTag = defaults.object(forKey: "viewTag") as? Int {
+            swipeView.tag = viewTag
+        }
+        swipeView.backgroundColor = color
+        swipeView.layer.cornerRadius = 10
+        swipeView.layer.shadowColor = color.cgColor
+        swipeView.applyCommonDropShadow(radius: 5, opacity: 1)
+        swipeView.alpha = 0
+        swipeView.frame = CGRect(x: (view.center.x)-(viewWidth/2), y: (view.center.y)-(viewHeight/2), width: viewWidth, height: viewHeight)
+        
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: UIView.AnimationOptions.curveEaseInOut, animations: { () -> Void in
+            swipeView.alpha = 1
+        })
+        let viewSwippedRight = UISwipeGestureRecognizer(target: self, action: #selector(viewIsSwipped(_:)))
+        viewSwippedRight.direction = .right
+        let viewSwippedLeft = UISwipeGestureRecognizer(target: self, action: #selector(viewIsSwipped(_:)))
+        viewSwippedRight.direction = .left
+        swipeView.addGestureRecognizer(viewSwippedRight)
+        swipeView.addGestureRecognizer(viewSwippedLeft)
+        return swipeView
+    }
+    
+    @objc func viewIsSwipped(_ sender: UISwipeGestureRecognizer){
+        let currentSwipeView = sender.view!
+        var currentSwipeFrame = currentSwipeView.frame
+        let defaults = UserDefaults.standard
+        let angle: CGFloat = 45.0 * CGFloat.pi / 180.0
+        
+        if let currentSwipeTag = defaults.object(forKey: "viewTag") as? Int{
+            defaults.set(currentSwipeTag+1, forKey: "viewTag")
+        }
+        let newSwipeView = addSwipeView()
+        newSwipeView.isUserInteractionEnabled = false
+        view.addSubview(newSwipeView)
+        
+        switch sender.direction {
+        case .left:
+            currentSwipeFrame.origin.x -= 70
+            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseInOut, animations: { [self] () -> Void in
+                view.bringSubviewToFront(currentSwipeView)
+                currentSwipeView.frame = currentSwipeFrame
+                let rotationMatrix = CGAffineTransform(rotationAngle: -angle)
+                let translationMatrix = CGAffineTransform(translationX: -100.0, y: -angle)
+                currentSwipeView.transform = translationMatrix.concatenating(rotationMatrix)
+            }) { completed in
+                UIView.animate(withDuration: 0.5, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: { () -> Void in
+                    currentSwipeView.alpha = 0
+                }) { completed in
+                    currentSwipeView.removeFromSuperview()
+                }
+                newSwipeView.isUserInteractionEnabled = true
+            }
+        case .right:
+            currentSwipeFrame.origin.x += 70
+            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseInOut, animations: { [self] () -> Void in
+                view.bringSubviewToFront(currentSwipeView)
+                currentSwipeView.frame = currentSwipeFrame
+                let rotationMatrix = CGAffineTransform(rotationAngle: angle)
+                let translationMatrix = CGAffineTransform(translationX: 100.0, y: 0.0)
+                currentSwipeView.transform = translationMatrix.concatenating(rotationMatrix)
+            }) { completed in
+                UIView.animate(withDuration: 0.5, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: { () -> Void in
+                    currentSwipeView.alpha = 0
+                }) { completed in
+                    currentSwipeView.removeFromSuperview()
+                }
+                newSwipeView.isUserInteractionEnabled = true
+            }
+        default:
+            break
+        }
+    }
+}
 
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
 
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+            red:   .random(),
+            green: .random(),
+            blue:  .random(),
+            alpha: 1.0
+        )
+    }
+}
+
+extension UIView {
+    func applyCommonDropShadow(radius:CGFloat, opacity: Float) {
+        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        layer.borderColor = UIColor.black.cgColor
+        clipsToBounds = false
+    }
 }
 
