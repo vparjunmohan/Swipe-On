4d3a8b87edd4ee32a7e7bcca2222f2d41dba78ed
Arjun Mohan <62532677+vparjunmohan@users.noreply.github.com>
2022-10-29 04:04:08 +0530

    initial commit

---
+++ Swipe On/Swipe On/SwipeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SwipeViewController.swift
+//  Swipe On
+//
+//  Created by Arjun Mohan on 29/10/22.
+//
+
+import UIKit
+
+class SwipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
